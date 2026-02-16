fe4c836794a8f2695bafb108ab61ee813a2c79ea
tworjaga <tworjaga@cherenkov.io>
2026-02-16 05:57:54 +0100

    feat: add React 18 entry point
    - Create root with ReactDOM.createRoot
    - Wrap App in React.StrictMode

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,12 @@
-import { render } from 'solid-js/web';
-import { Router } from '@solidjs/router';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-render(
-  () => (
-    <Router>
-      <App />
-    </Router>
-  ),
-  document.getElementById('root')!
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
